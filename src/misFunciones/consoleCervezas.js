0ee00adebc26e516eb540e82bbb7639276b4e00d
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2023-11-15 19:50:04 +0100

    .

--- src/misFunciones/consoleCervezas.js
+++ src/misFunciones/consoleCervezas.js
@@ -3,7 +3,5 @@ import { cervezasBD } from '../bd/cervezasBD.js'
 export const consoleCervezas = (cervezasBD) => {
   cervezasBD.forEach(element => {
     console.log('Nombre: ', element.nombre)
-    console.log('Grado: ', element.gradoAlcoholico)
-    console.log('Tipo: ', element.tipo)
   })
 }
