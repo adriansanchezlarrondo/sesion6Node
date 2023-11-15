30c6ce36228f83a953cf2d146a40b98a93f8e8c7
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2023-11-15 19:56:53 +0100

    .

--- src/main.js
+++ src/main.js
@@ -9,5 +9,3 @@ document.querySelector('header').innerHTML = header.template
 header.script()
 document.querySelector('main').innerHTML = vistaHome.template
 vistaHome.script()
-// document.querySelector('footer').innerHTML = footer.template
-// footer.script()
